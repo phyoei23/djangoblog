c65479ad37e10995bbcb80e76f94b8a8644d8862
phyoei23 <eep123kpg@gmail.com>
2026-09-11 15:06:28 +0900

    CSS,Base.html 2026-09-11

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'blog/home.html', {'title': 'Djangoblog Homepage.'})
+
+def about(request):
+    return render(request, 'blog/about.html', {'about': 'Djangoblog team.'})
+
+def content(request):
+    return render(request, 'blog/content.html', {'content': 'Djangoblog team.'})
