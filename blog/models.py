2e8d9f7b0209271e72fe0979b8364251ec4c01d2
phyoei23 <eep123kpg@gmail.com>
2026-09-14 14:34:36 +0900

    Update project

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class POst(models.Model):
+    STATUS_CHOICES = [
+        ('draft','Draft'),('published','Published')
+    ]
     titel = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='published')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
